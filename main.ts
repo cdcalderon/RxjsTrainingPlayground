21a09a2b20b91e5c10715b2fce1465b31bdddfd3
carloscalderon <cdcalderon@gmail.com>
2018-01-14 17:25:37 -0600

    Implement Follow circle

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import {Observable} from "rxjs";
 
 let numbers = [1, 2, 5, 6];
 //let source = Observable.from(numbers);
-
+let circle = document.getElementById("circle")
 let source = Observable.fromEvent(document, "mousemove")
                        .map((e : MouseEvent) => {
                          return {
@@ -11,9 +11,15 @@ let source = Observable.fromEvent(document, "mousemove")
                          };
                        })
                        .filter(value => value.x > 500)
+                       .delay(300)
+
+function onNext(value) {
+  circle.style.left = value.x;
+  circle.style.top = value.y;
+}
 
 source.subscribe(
-  (value) => console.log(`value: ${value}`),
+  onNext,
   (e) => console.log(`error ${e}`),
   () => console.log("complete")
 );
