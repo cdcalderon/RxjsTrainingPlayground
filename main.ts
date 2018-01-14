e7e72c30d3befd0211bdc3135f66bdb0b498b55b
carloscalderon <cdcalderon@gmail.com>
2018-01-14 17:18:29 -0600

    add observable.fromEvent

--- main.ts
+++ main.ts
@@ -3,21 +3,14 @@ import {Observable} from "rxjs";
 let numbers = [1, 2, 5, 6];
 //let source = Observable.from(numbers);
 
-let source = Observable.create(observer => {
-  let index = 0;
-  let produceValue = () => {
-    observer.next(numbers[index++]); 
-
-    if(index < numbers.length) {
-      setTimeout(produceValue, 2000);
-    } else {
-      observer.complete();
-    }
-  }
-  produceValue();
-}).map(n => n * 2)
-.filter(n => n > 4);
-
+let source = Observable.fromEvent(document, "mousemove")
+                       .map((e : MouseEvent) => {
+                         return {
+                           x: e.clientX,
+                           y: e.clientY
+                         };
+                       })
+                       .filter(value => value.x > 500)
 
 source.subscribe(
   (value) => console.log(`value: ${value}`),
