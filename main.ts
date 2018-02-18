f90271e1e94571c0795f1e8aa0943f9bf2bbc365
carloscalderon <cdcalderon@gmail.com>
2018-02-18 00:16:18 -0600

    2 observables StartButton and interval Right way with SwitchMap - Refactored joining Observables

--- main.ts
+++ main.ts
@@ -4,9 +4,10 @@ const startButton = document.querySelector('#start');
 
 const start$ = Observable.fromEvent(startButton, 'click');
 const interval$ = Observable.interval(1000);
+const startInterval$ = start$
+    .switchMap((event) => interval$);
 
-start$
-    .switchMap((event) => interval$)
+startInterval$
     .subscribe((x) => console.log(x));
 
 
