892d4c9d76c0fd205ac7a2f98948ad7406ba2d55
carloscalderon <cdcalderon@gmail.com>
2018-02-25 20:57:46 -0600

    Add Stop Button in order to implement takeUntil Operator

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 import { Observable } from 'rxjs';
 
 const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
 
 const start$ = Observable.fromEvent(startButton, 'click');
+const stop$ = Observable.fromEvent(stopButton, 'click');
+
 const interval$ = Observable.interval(1000);
+
 const startInterval$ = start$
     .switchMapTo(interval$);
 
